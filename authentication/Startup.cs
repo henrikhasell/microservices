e2eda8cd172a92adb95b0d11b7a38ebc3742b6ae
Henrik Hasell <henrik@prosysdev.com>
2018-05-30 17:28:07 +0100

    Added notes.

--- authentication/Startup.cs
+++ authentication/Startup.cs
@@ -1,10 +1,14 @@
 using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+
+// https://amanagrawal.blog/2017/09/18/jwt-token-authentication-with-cookies-in-asp-net-core/
+// https://stormpath.com/blog/token-authentication-asp-net-core
 
 namespace Authentication
 {
@@ -19,7 +23,13 @@ namespace Authentication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie();
+                /*
                 .AddJwtBearer(options =>
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -33,6 +43,7 @@ namespace Authentication
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
                     };
                 });
+                */
         }
         
         public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
